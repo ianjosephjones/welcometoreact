e07bd89535eddd8571488a765a6a273fc4c10f96
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-07-12 20:08:32 -0400

    Added props and states

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Car extends React.Component {
+	// To declare a var in a constructor "this"
+	constructor() {
+		super();
+		// internal components
+		this.state = { color: 'red' };
+	}
 	render() {
-		return <h2>Hi, I am a Car</h2>;
+		return (
+			<h2>
+				Hi, I am a {this.state.color} Car made by {this.props.make}
+			</h2>
+		);
 	}
 }
 
-ReactDOM.render(<Car />, document.getElementById('root'));
+ReactDOM.render(<Car make="Ford" />, document.getElementById('root'));
